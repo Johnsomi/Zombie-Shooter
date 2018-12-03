36f5ccff021ce829bfe1409a790458fd23b2508e
Johnsomi <mj4100@students.inghamisd.org>
2018-12-03 14:09:23 -0500

    dht

---
+++ Sprite Collision/Sprite Collision/Game1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Sprite_Collision.Models;
+using Sprite_Collision.Sprites;
+using System.Collections.Generic;
+
+namespace Sprite_Collision
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+
+        private List<Sprite> _sprites;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            ScreenWidth = graphics.PreferredBackBufferWidth;
+            ScreenHeight = graphics.PreferredBackBufferHeight;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var playerTexture = Content.Load<Texture2D>("pacmanWS");
+
+            _sprites = new List<Sprite>()
+            {
+                new Player(playerTexture)
+                {
+                    Input = new Input()
+                    {
+                        Left = Keys.A,
+                        Right = Keys.D,
+                        Up = Keys.W,
+                        Down = Keys.S,
+                    },
+                    Position = new Vector2(100, 100),
+                    color = Color.Blue,
+                    Speed = 5f,
+                },
+                new Player(playerTexture)
+                {
+                    Input = new Input()
+                    {
+                        Left = Keys.Left,
+                        Right = Keys.Right,
+                        Up = Keys.Up,
+                        Down = Keys.Down,
+                    },
+                    Position = new Vector2(300, 100),
+                    color = Color.Red,
+                    Speed = 5f,
+                },
+            };
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Update(gameTime, _sprites);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            foreach (var sprite in _sprites)
+                sprite.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Sprite Collision/Sprite Collision/Sprites/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprite_Collision.Sprites
+{
+    public class Player : Sprite
+    {
+        public Player(Texture2D texture)
+            : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            Move();
+
+            foreach(var sprite in sprites)
+            {
+                if (sprite == this)
+                    continue;
+
+                if ((this.Velocity.X > 0 && this.IsTouchingLeft(sprite)) ||
+                    (this.Velocity.X < 0 && this.IsTouchingRight(sprite)))
+                    this.Velocity.X = 0;
+
+                if ((this.Velocity.Y > 0 && this.IsTouchingTop(sprite)) ||
+                    (this.Velocity.Y < 0 && this.IsTouchingBottom(sprite)))
+                     this.Velocity.Y = 0;
+
+                /*if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite))
+                    this.Velocity.X = 0;
+                if (this.Velocity.X < 0 && this.IsTouchingRight(sprite))
+                    this.Velocity.X = 0;
+                */
+            }
+
+            Position += Velocity;
+
+            Position.X = MathHelper.Clamp(Position.X, 0, Game1.ScreenWidth - Rectangle.Width);
+            Position.Y = MathHelper.Clamp(Position.Y, 0, Game1.ScreenHeight - Rectangle.Height);
+
+
+            Velocity = Vector2.Zero;
+        }
+
+        private void Move()
+        {
+            if (Keyboard.GetState().IsKeyDown(Input.Left))
+                Velocity.X = -Speed;
+            if (Keyboard.GetState().IsKeyDown(Input.Right))
+                Velocity.X = Speed;
+
+            if (Keyboard.GetState().IsKeyDown(Input.Up))
+                Velocity.Y = -Speed;
+            if (Keyboard.GetState().IsKeyDown(Input.Down))
+                Velocity.Y = Speed;
+        }
+    }
+}

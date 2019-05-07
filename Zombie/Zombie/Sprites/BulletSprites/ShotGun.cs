f4294d5c245c609e6053693adbdb9a219a7fb709
Johnsomi <mj4100@students.inghamisd.org>
2019-05-07 11:55:42 -0400

    x

---
+++ Zombie/Zombie/Sprites/BulletSprites/ShotGun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zombie.Sprites.BulletSprites
+{
+    public class ShotGun : Bullet
+    {
+        public ShotGun(Texture2D texture) : base(texture)
+        {
+            Color = Color.Red;
+            bulletDamage = 1;
+            LifeSpan = 1f;
+            LinearVelocity = 10f * changedBulletVelocity;
+        }
+    }
+}
